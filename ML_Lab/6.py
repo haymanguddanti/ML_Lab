000a8819759b81c9f99a927743e143238b4b78dc
Hayman Guddanti <52147120+haymanguddanti@users.noreply.github.com>
2021-07-31 22:00:01 +0530

    Update 6.py

--- ML_Lab/6.py
+++ ML_Lab/6.py
@@ -14,17 +14,21 @@ def estimate_coef(x, y):
 
 
 def plot_regression_line(x, y, b):
-    plt.scatter(x, y, color="m",    marker="o", s=30)
+    plt.scatter(x, y, color="m", marker="o", s=30)
+    y_pred = b[0] + b[1]*x
+    plt.plot(x, y_pred, color = "g")
     plt.xlabel('x')
     plt.ylabel('y')
+    plt.show()
 
 
 def main():
     x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
     y = np.array([1, 3, 2, 5, 7, 8, 8, 9, 10, 12])
     b = estimate_coef(x, y)
-    print("Estimated coefficients:\nb_0 = {} \ \nb_1 = {}".format(b[0], b[1]))
+    print("Estimated coefficients:\nb_0 = {} \nb_1 = {}".format(b[0], b[1]))
+    plot_regression_line(x, y, b)
 
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     main()
